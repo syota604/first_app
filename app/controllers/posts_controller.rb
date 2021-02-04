42ffa61f7202f47f2a22d470844b523884e76507
syota604 <78452908+syota604@users.noreply.github.com>
2021-02-04 20:25:56 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+
+  def index # indexsアクションを定義した
+    @posts = Post.all  # すべてのレコードを@postsに代入
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
